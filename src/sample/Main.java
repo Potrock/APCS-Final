f16f186523594dcf7df16dae052e6345d245700f
Patrick <pwblais@icloud.com>
2018-05-09 12:38:38 -0400

    Deleted broken OpenCV code from screenshot feature

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -27,72 +27,10 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        new Process().analyze();
     }
 
 
     public static void main(String[] args) {
         launch(args);
-    }
-}
-
-class Process{
-    void analyze(){
-
-        URL img_url = getClass().getResource("HQquestion.png");
-        String img_path = img_url.getPath();
-        Mat Main = Imgcodecs.imread(img_path);
-        Mat rgb = new Mat();
-
-        Imgproc.pyrDown(Main, rgb);
-
-        Mat small = new Mat();
-
-        Imgproc.cvtColor(rgb, small, Imgproc.COLOR_RGB2GRAY);
-
-        Mat grad = new Mat();
-
-        Mat morphKernel = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(3,3));
-
-        Imgproc.morphologyEx(small, grad, Imgproc.MORPH_GRADIENT , morphKernel);
-
-        Mat bw = new Mat();
-
-        Imgproc.threshold(grad, bw, 0.0, 255.0, Imgproc.THRESH_OTSU);
-
-        Mat connected = new Mat();
-
-        morphKernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(9, 1));
-
-        Imgproc.morphologyEx(bw, connected, Imgproc.MORPH_CLOSE  , morphKernel);
-
-
-        Mat mask = Mat.zeros(bw.size(), CvType.CV_8UC1);
-
-        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-
-        Mat hierarchy = new Mat();
-
-        Imgproc.findContours(connected, contours, hierarchy, Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE, new Point(0, 0));
-
-        for(int idx = 0; idx < contours.size(); idx++)
-        {
-            Rect rect = Imgproc.boundingRect(contours.get(idx));
-
-            Mat maskROI = new Mat(mask, rect);
-            maskROI.setTo(new Scalar(0, 0, 0));
-
-            Imgproc.drawContours(mask, contours, idx, new Scalar(255, 255, 255), Core.FILLED);
-
-            double r = (double)Core.countNonZero(maskROI)/(rect.width*rect.height);
-
-            if (r > .45 && (rect.height > 8 && rect.width > 8))
-            {
-                Imgproc.rectangle(rgb, rect.br() , new Point( rect.br().x-rect.width ,rect.br().y-rect.height),  new Scalar(0, 255, 0));
-            }
-
-            String outputfile = "outlined.png";
-            Imgcodecs.imwrite(outputfile,rgb);
-        }
     }
 }
