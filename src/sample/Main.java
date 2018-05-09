e2fde932728a7bf73b1a6603a80f5fd652ad4cfc
Sawyer <sawyercoletang@gmail.com>
2018-05-09 19:14:29 -0400

    Updated Toolbar added: - LoginPage.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Separator;
@@ -10,6 +11,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import sample.UIElements.Page.MainMenuPage;
 
@@ -21,16 +24,22 @@ public class Main extends Application {
         BorderPane layout_pn = new BorderPane();
         Scene main_scn = new Scene(layout_pn);
 
-        main_scn.getStylesheets().addAll("sample/UIElements/Page/MainMenu.css");
+        main_scn.getStylesheets().addAll("sample/UIElements/Page/PageElements.css", "sample/UIElements/UI/UI.css");
 
         //Top
         ImageView imgView = new ImageView(new Image("sample/UIElements/UI/logo.png"));
-        imgView.setFitWidth(25);
-        imgView.setFitHeight(25);
-        imgView.setId("imgView");
+        imgView.setFitWidth(30);
+        imgView.setFitHeight(30);
+        imgView.setId("button_toolbar");
+        imgView.setOnMousePressed(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent t) {
+               // --> Home
+            }
+        });
 
         Button quit_btn = new Button("Quit");
-        quit_btn.setId("test");
+        quit_btn.setId("button_toolbar");
         quit_btn.setOnMousePressed(new EventHandler<MouseEvent>(){
             @Override
             public void handle(MouseEvent t) {
@@ -39,28 +48,27 @@ public class Main extends Application {
         });
 
         Button menu_btn = new Button("Menu");
+        menu_btn.setId("button_toolbar");
         menu_btn.setOnMousePressed(new EventHandler<MouseEvent>(){
             @Override
             public void handle(MouseEvent t) {
-                ///---///
+                // --> Menu
             }
         });
 
-        ToolBar toolBar = new ToolBar();
-        toolBar.getItems().addAll(imgView, new Separator(), quit_btn,  menu_btn);
+        ToolBar toolBar = new ToolBar(imgView, new Separator(), menu_btn, quit_btn);
 
         //Center
         MainMenuPage menuScene = new MainMenuPage();
 
+        //
         layout_pn.setTop(toolBar);
         layout_pn.setCenter(menuScene.get());
-
 
         primaryStage.setTitle("HQ Cheat");
         primaryStage.setScene(main_scn);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
---
+++ src/sample/UIElements/Page/LoginPage.java
@@ -0,0 +1,17 @@
+package sample.UIElements.Page;
+
+public class LoginPage extends PageElement{
+
+    public LoginPage() {
+        super();
+            
+
+
+        setStyles();
+    }
+
+    void setStyles(){
+
+    }
+
+}
