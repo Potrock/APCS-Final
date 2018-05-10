5b7b66c26f9e34debea13d5ffd865ee56fc876c7
Patrick <pwblais@icloud.com>
2018-05-09 21:33:49 -0400

    OpenCV - Tesseract - All working
    Got OpenCV working for turning image into Greyscale and applying gaussian blur and an adaptive threshold, found crop values that work well for the test image (719x1280) that detect question, and another that detect answers (doesn't work in one go unfortunately). Filter out leading chars from answer, save them to an array, and print everything :D

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,9 +7,16 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
-import net.sourceforge.tess4j.TesseractException;
 import net.sourceforge.tess4j.util.LoadLibs;
+import nu.pattern.OpenCV;
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 
 
@@ -17,20 +24,48 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // System.setProperty("jna.library.path", "32".equals(System.getProperty("sun.arch.data.model")) ? "lib/win32-x86" : "lib/win32-x86-64");
+         System.setProperty("jna.library.path", "32".equals(System.getProperty("sun.arch.data.model")) ? "lib/win32-x86" : "lib/win32-x86-64");
 
-        File imageFile = new File("HQquestion.png");
-        ITesseract instance = new Tesseract();  // JNA Interface Mapping
-        // ITesseract instance = new Tesseract1(); // JNA Direct Mapping
-         File tessDataFolder = LoadLibs.extractTessResources("tessdata"); // Maven build bundles English data
-         instance.setDatapath(tessDataFolder.getPath());
+        OpenCV.loadLocally();
+        Mat img = Imgcodecs.imread("HQquestion.png");
 
-        try {
-            String result = instance.doOCR(imageFile);
-            System.out.println(result);
-        } catch (TesseractException e) {
-            System.err.println(e.getMessage());
+        Mat imgGray = new Mat();
+        Imgproc.cvtColor(img, imgGray, Imgproc.COLOR_BGR2GRAY);
+        Imgcodecs.imwrite("preprocess/img.png", imgGray);
+
+        Mat imgGaussianBlur = new Mat();
+        Imgproc.GaussianBlur(imgGray,imgGaussianBlur,new Size(3, 3),0);
+        Imgcodecs.imwrite("preprocess/img.png", imgGaussianBlur);
+
+        Mat imgAdaptiveThreshold = new Mat();
+        Imgproc.adaptiveThreshold(imgGaussianBlur, imgAdaptiveThreshold, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C ,Imgproc.THRESH_BINARY, 99, 4);
+        Imgcodecs.imwrite("preprocess/img.png", imgAdaptiveThreshold);
+        File imageFile = new File("preprocess/img.png");
+        int x = 0, y = 200, w = 719, h = 250;
+        BufferedImage dst = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+        Image src = ImageIO.read(new File("preprocess/img.png"));
+        dst.getGraphics().drawImage(src, 0, 0, w, h, x, y, x + w, y + h, null);
+        ImageIO.write(dst, "png", new File("preprocess/img.png"));
+        ITesseract instance = new Tesseract();
+        File tessDataFolder = LoadLibs.extractTessResources("tessdata"); // Maven build bundles English data
+        instance.setDatapath(tessDataFolder.getPath());
+        instance.setLanguage("eng");
+        String question = instance.doOCR(imageFile);
+        x = 65; y = 500; w = 500; h = 1280/4;
+        dst = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+        dst.getGraphics().drawImage(src, 0, 0, w, h, x, y, x + w, y + h, null);
+        ImageIO.write(dst, "png", new File("preprocess/img.png"));
+        String[] answers = instance.doOCR(imageFile).split("\n");
+
+        System.out.println(question);
+        //Filter out lowercase L and spaces from front of answers (shitty OCR tbh, oh well) and print them.
+        for(String i : answers) {
+            if (i.charAt(0) == 'l') {
+                i = i.substring(2);
+                System.out.println(i);
+            }
         }
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
